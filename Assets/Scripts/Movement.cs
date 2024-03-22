ba63cb65f5f9d6bd980c36ebb094bb493bea81a9
IkerVeiga <veiga.iker@opendeusto.es>
2024-03-22 12:11:51 +0100

    la camara funciona

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,8 +8,9 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
    
     InputActionMap inputActionMap;
-    public GameObject characterModel;
+    public GameObject camera;
     public float speed;
+    public Transform characterDirection;
     //public GameObject camera;
     void Start()
     {
@@ -20,20 +21,31 @@ public class Movement : MonoBehaviour
     void Update()
     {
         manageHorizontalMovement();
+        manageCamera();
+        Debug.Log(transform.forward);
     }
 
     private void manageHorizontalMovement()
     {
         Vector2 axis = inputActionMap.FindAction("HorizontalMovement").ReadValue<Vector2>();
-        transform.Translate(new Vector3(axis.x* Time.deltaTime * speed, 0.0f, axis.y * Time.deltaTime * speed));
+        Vector3 step = new Vector3(axis.x * Time.deltaTime * speed, 0.0f, axis.y * Time.deltaTime * speed);
+        transform.Translate(step);
         
     }
 
     private void manageCamera()
     {
-        Vector3 viewDirection = new Vector3(characterModel.transform.position.x - this.transform.position.x, 0, characterModel.transform.position.z - this.transform.position.z);
+        Vector3 viewDirection = new Vector3(this.transform.position.x - camera.transform.position.x, 0, this.transform.position.z - camera.transform.position.z);
         viewDirection = viewDirection.normalized;
-        float angle = Vector3.Angle(Vector3.forward, viewDirection);
-
+        float angle2 = Mathf.Atan2(viewDirection.x, viewDirection.z) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.Euler(0, angle2, 0);
+        //float angle = Vector3.Angle(viewDirection, transform.forward);
+        //Debug.Log(angle);
+        //characterDirection.Rotate(Vector3.up, angle);
+        //if (angle > Mathf.PI)
+        //{
+        //    angle -= Mathf.PI;
+        //}
+        //transform.Rotate(Vector3.up, angle);
     }
 }
