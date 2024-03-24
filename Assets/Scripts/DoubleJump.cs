6a53dfb11f93425d1ebc76dc1630a5a95b8757bc
IkerVeiga <veiga.iker@opendeusto.es>
2024-03-24 13:35:19 +0100

    doble salto

---
+++ Assets/Scripts/DoubleJump.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DoubleJump : MonoBehaviour
+{
+    private Rigidbody physics;
+    private bool grounded;
+    private bool canDoubleJump;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        physics = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+        if (grounded)
+            {
+                physics.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                canDoubleJump = true;
+            
+            } else if(canDoubleJump)
+            {
+                physics.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                canDoubleJump = false;
+            }
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        grounded = false;
+    }
+}
