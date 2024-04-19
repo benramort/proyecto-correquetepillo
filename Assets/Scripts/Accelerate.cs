84ee92a540efbb9422239ec6e22a89fcce3531c0
IkerVeiga <veiga.iker@opendeusto.es>
2024-04-19 12:08:23 +0200

    activa de acelerar

---
+++ Assets/Scripts/Accelerate.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Accelerate : MonoBehaviour
+{
+    Movement movement;
+    Coroutine coroutine;
+    public int multiplier;
+    public float abilityTime;
+    public float cooldownTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator AccelerateCoroutine()
+    {
+        movement.speed = movement.speed * multiplier;
+        yield return new WaitForSeconds(abilityTime);
+        movement.speed /= multiplier;
+        yield return new WaitForSeconds(cooldownTime);
+        coroutine = null;
+    }
+
+    public void UseHability(InputAction.CallbackContext context)
+    {
+        if(coroutine == null)
+        {
+            coroutine = StartCoroutine(AccelerateCoroutine());
+        }
+    }
+
+
+}
