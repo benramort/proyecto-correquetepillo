2818698fb996b20717e494cafdee3b89ea12274f
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2025-05-30 10:12:06 +0200

    Cambios a los prefabs

--- Assets/Scripts/Items/SurfaceGrenade.cs
+++ Assets/Scripts/Items/SurfaceGrenade.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class SurfaceGrenade : Item
 {
-    public GameObject surface;
+    public NetworkObject surface;
     public override void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag != "Player") //Esto igual hacerlo ==map?
+        if (!IsOwner) return;
+        if (collision.gameObject.tag != "Player")
         {
             ContactPoint cp = collision.GetContact(0);
             if (cp.normal.y > 0.9)
             {
-                Instantiate(surface, cp.point, Quaternion.identity);
-                Destroy(this.gameObject);
+                NetworkManager.SpawnManager.InstantiateAndSpawn(surface, position:cp.point, rotation:Quaternion.identity);
+                GetComponent<NetworkObject>().Despawn(true);
             }
         }
 
